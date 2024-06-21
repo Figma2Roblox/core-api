2ba68cc3c5691dcc157352087bd3472f25ed3da7
zoebasil <dog456pop@gmail.com>
2024-06-21 01:36:56 -0600

    Refactor & bug fix, prettify for first iteration

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
-import { GetApperanceRuleReturnType, GetElementRuleReturnType, RuleConfig, RuleSignature, signatureSymbol, TypeEvaluator } from "./types/rules"
+import {
+	GetAppearanceRuleReturnType,
+	GetElementRuleReturnType,
+	RuleConfig,
+	RuleSignature,
+	signatureSymbol,
+	TypeEvaluator,
+} from "./types/rules";
 
 function upperFirst(str: string): string {
-	return str.charAt(0).toUpperCase() + str.slice(1)
+	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 type InternalRule = {
-	ruleType: "element" | "appearance",
-	name: string,
-	evaluator: (...args: any[]) => any,
-	after?: string[],
-	before?: string[],
-}
+	ruleType: "element" | "appearance";
+	name: string;
+	evaluator: (...args: any[]) => any;
+	signatureId: string;
+	dependencies?: Set<string>;
+};
 
-const anyRuleBySignature: { [key: string]: InternalRule } = {}
-const elementRuleBySignature: { [key: string]: InternalRule } = {}
-const appearanceRuleGroups: { [key: string]: {[key: string]: InternalRule} } = {}
+export const anyRuleBySignature: { [key: string]: InternalRule } = {};
+export const elementRuleBySignature: { [key: string]: InternalRule } = {};
+export const appearanceRuleGroups: { [key: string]: { [key: string]: InternalRule } } = {};
 
 function createRuleSingature(name: string, appearanceGroup?: string): RuleSignature {
 	return {
@@ -22,86 +29,218 @@ function createRuleSingature(name: string, appearanceGroup?: string): RuleSignat
 		symbol: signatureSymbol,
 		signatureId: crypto.randomUUID(),
 		appearanceGroup: appearanceGroup,
-	}
+	};
 }
 
-function checkDependencies(errorVerb: string, ruleType: "element" | "appearance", thisRuleName: string, dependencies: RuleSignature[]) {
-	const otherType = ruleType === "element" ? "appearance" : "element"
+function simpleCheckDependenices(
+	errorVerb: string,
+	ruleType: "element" | "appearance",
+	thisRuleName: string,
+	dependencies: RuleSignature[],
+) {
+	const otherType = ruleType === "element" ? "appearance" : "element";
 
 	for (const signature of dependencies) {
-		const rule = elementRuleBySignature[signature.signatureId]
-		const errorPrefix = `${upperFirst(ruleType)} rule '${thisRuleName}' cannot be evaluated ${errorVerb} rule '${signature.name}': `
+		const rule = anyRuleBySignature[signature.signatureId];
+		const errorPrefix = `${upperFirst(ruleType)} rule '${thisRuleName}' cannot be evaluated ${errorVerb} rule '${signature.name}': `;
 
 		if (rule === undefined) {
-			throw new Error(errorPrefix + `'${signature.name}' does not exist. (It's possible these rules are not using the same core-api module. Check to make sure your peerDependencies in package.json is configured correctly.)`)
+			throw new Error(
+				errorPrefix +
+					`'${signature.name}' does not exist. (It's possible these rules are not using the same core-api module. Check to make sure your peerDependencies in package.json is configured correctly.)`,
+			);
 		} else if (rule.ruleType !== ruleType) {
-			throw new Error(errorPrefix + `'${signature.name}' is an ${otherType} rule. ${upperFirst(ruleType)} rules can only be evaluated ${errorVerb} other ${ruleType} rules.`)
+			throw new Error(
+				errorPrefix +
+					`'${signature.name}' is an ${otherType} rule. ${upperFirst(ruleType)} rules can only be evaluated ${errorVerb} other ${ruleType} rules.`,
+			);
 		}
 	}
 }
- 
-function AddElementRule<Config extends RuleConfig>(
-	config: Config,
-	evaluator: TypeEvaluator<Config, GetElementRuleReturnType<Config>>
-): RuleSignature {
-	if ("evaluateAfter" in config && config.evaluateAfter !== undefined) {
-		checkDependencies("after", "element", config.name, config.evaluateAfter)
-	} else if ("evaluateBefore" in config && config.evaluateBefore !== undefined) {
-		checkDependencies("before", "element", config.name, config.evaluateBefore)
+
+function checkGroupDependencies(
+	errorVerb: string,
+	thisRuleName: string,
+	thisRuleGroup: string,
+	dependencies: RuleSignature[],
+) {
+	for (const signature of dependencies) {
+		if (signature.appearanceGroup !== thisRuleGroup) {
+			throw new Error(
+				`Appearance rule '${thisRuleName}' cannot be evaluated ${errorVerb} rule '${signature.name}': '${thisRuleName}' belongs to appearance group '${thisRuleGroup}', while '${signature.name}' belongs to appearance group '${signature.appearanceGroup}'`,
+			);
+		}
+	}
+}
+
+function processDependencies(config: RuleConfig, ruleType: "element" | "appearance", signature: RuleSignature) {
+	const dependencies: Set<string> | undefined = config.evaluateAfter
+		? new Set(config.evaluateAfter?.map(sig => sig.signatureId))
+		: undefined;
+
+	config.evaluateBefore?.forEach(dependentSig => {
+		if (dependencies?.has(dependentSig.signatureId)) {
+			throw new Error(
+				`${upperFirst(ruleType)} rule '${config.name}' cannot be evaluated before and after rule '${dependentSig.name}'`,
+			);
+		}
+
+		const dependentRule = anyRuleBySignature[dependentSig.signatureId];
+		if (!dependentRule.dependencies) {
+			dependentRule.dependencies = new Set([signature.signatureId]);
+		} else if (!dependentRule.dependencies.has(signature.signatureId)) {
+			dependentRule.dependencies.add(signature.signatureId);
+		}
+	});
+
+	return dependencies;
+}
+
+function hasCircularDependency(ruleId: string, visited: Set<string>, stack: Set<string>): boolean {
+	if (stack.has(ruleId)) {
+		return true; // Cycle detected
 	}
 
-	const signature = createRuleSingature(config.name)
+	if (visited.has(ruleId)) {
+		return false; // Already visited, no cycle from this node
+	}
+
+	visited.add(ruleId);
+	stack.add(ruleId);
+
+	const evaluationDepedencySet = anyRuleBySignature[ruleId].dependencies;
+
+	for (const dependencyId of evaluationDepedencySet || []) {
+		if (hasCircularDependency(dependencyId, visited, stack)) {
+			return true;
+		}
+	}
+
+	stack.delete(ruleId); // Backtrack
+	return false;
+}
+
+export function addElementRule<Config extends RuleConfig>(
+	config: Config,
+	evaluator: TypeEvaluator<Config, GetElementRuleReturnType<Config>>,
+): RuleSignature {
+	if ("evaluateAfter" in config && config.evaluateAfter) {
+		simpleCheckDependenices("after", "element", (config as Config).name, config.evaluateAfter);
+	} else if ("evaluateBefore" in config && config.evaluateBefore !== undefined) {
+		simpleCheckDependenices("before", "element", config.name, config.evaluateBefore);
+	}
+
+	const signature = createRuleSingature(config.name);
 	const internalRule: InternalRule = {
 		name: config.name,
 		ruleType: "element",
-		after: config.evaluateAfter?.map(sig => sig.signatureId),
-		before: config.evaluateBefore?.map(sig => sig.signatureId),
-		evaluator: evaluator
-	}
+		evaluator: evaluator,
+		signatureId: signature.signatureId,
+		dependencies: processDependencies(config, "element", signature),
+	};
 
-	anyRuleBySignature[signature.signatureId] = internalRule
-	elementRuleBySignature[signature.signatureId] = internalRule
-	
-	return signature
+	anyRuleBySignature[signature.signatureId] = internalRule;
+	elementRuleBySignature[signature.signatureId] = internalRule;
+
+	return signature;
 }
 
-function checkGroupDependencies(errorVerb: string, thisRuleName: string, thisRuleGroup: string, dependencies: RuleSignature[]) {
-	for (const signature of dependencies) {
-		if (signature.appearanceGroup !== thisRuleGroup) {
-			throw new Error(`Appearance rule '${thisRuleName}' cannot be evaluated after rule '${signature.name}': '${thisRuleName}' belongs to appearance group '${thisRuleGroup}', while '${signature.name}' belongs to appearance group '${signature.appearanceGroup}'`)
-		}
-	}
-}
-
-function AddAppearanceRule<Config extends RuleConfig>(
+export function addAppearanceRule<Config extends RuleConfig>(
 	groupName: string,
 	config: Config,
-	evaluator: TypeEvaluator<Config, GetApperanceRuleReturnType<Config>>
+	evaluator: TypeEvaluator<Config, GetAppearanceRuleReturnType<Config>>,
 ): RuleSignature {
-	if (!(groupName in appearanceRuleGroups)) {
-		appearanceRuleGroups[groupName] = {}
-	}
-	const appearanceRuleBySignature = appearanceRuleGroups[groupName]
-
 	if ("evaluateAfter" in config && config.evaluateAfter !== undefined) {
-		checkDependencies("after", "appearance", config.name, config.evaluateAfter)
-		checkGroupDependencies("after", config.name, groupName, config.evaluateAfter)
+		simpleCheckDependenices("after", "appearance", config.name, config.evaluateAfter);
+		checkGroupDependencies("after", config.name, groupName, config.evaluateAfter);
 	} else if ("evaluateBefore" in config && config.evaluateBefore !== undefined) {
-		checkDependencies("before", "appearance", config.name, config.evaluateBefore)
-		checkGroupDependencies("before", config.name, groupName, config.evaluateBefore)
+		simpleCheckDependenices("before", "appearance", config.name, config.evaluateBefore);
+		checkGroupDependencies("before", config.name, groupName, config.evaluateBefore);
 	}
 
-	const signature = createRuleSingature(config.name, groupName)
+	const signature = createRuleSingature(config.name, groupName);
 	const internalRule: InternalRule = {
 		name: config.name,
 		ruleType: "appearance",
-		after: config.evaluateAfter?.map(sig => sig.signatureId),
-		before: config.evaluateBefore?.map(sig => sig.signatureId),
-		evaluator: evaluator
+		evaluator: evaluator,
+		signatureId: signature.signatureId,
+		dependencies: processDependencies(config, "appearance", signature),
+	};
+
+	anyRuleBySignature[signature.signatureId] = internalRule;
+
+	if (!appearanceRuleGroups[groupName]) {
+		appearanceRuleGroups[groupName] = {};
+	}
+	const appearanceRuleBySignature = appearanceRuleGroups[groupName];
+	appearanceRuleBySignature[signature.signatureId] = internalRule;
+
+	return signature;
+}
+
+export function orderRulesByDependencies(unorderedRules: InternalRule[]): InternalRule[] {
+	// Check for circular dependencies
+	const visited = new Set<string>();
+	const stack = new Set<string>();
+	for (const rule of unorderedRules) {
+		if (hasCircularDependency(rule.signatureId, visited, stack)) {
+			let cycleString = "";
+			let hasStarted = false;
+			for (const ruleId of stack) {
+				if (rule.signatureId === ruleId && !hasStarted) {
+					hasStarted = true;
+				}
+				if (hasStarted) {
+					cycleString += `'${anyRuleBySignature[ruleId]?.name}' after -> `;
+				}
+			}
+			cycleString += `'${rule.name}'`;
+
+			throw new Error(`Circular dependency detected: [${cycleString}]`);
+		}
 	}
 
-	anyRuleBySignature[signature.signatureId] = internalRule
-	elementRuleBySignature[signature.signatureId] = internalRule
+	const orderedRules: InternalRule[] = [];
+	const ruleVisitedById: { [key: string]: true } = {};
 
-	return signature
+	while (orderedRules.length < unorderedRules.length) {
+		for (const rule of unorderedRules) {
+			if (ruleVisitedById[rule.signatureId]) {
+				continue;
+			}
+
+			let dependenciesOrdered = true;
+
+			if (rule.dependencies) {
+				for (const dependencyId of rule.dependencies) {
+					if (!ruleVisitedById[dependencyId]) {
+						dependenciesOrdered = false;
+						break;
+					}
+				}
+			}
+
+			if (dependenciesOrdered) {
+				ruleVisitedById[rule.signatureId] = true;
+				orderedRules.push(rule);
+				// Once this rule is ordered we want to start from the beginning to order rules that have a dependency on this rule
+				break;
+			}
+		}
+	}
+
+	console.log("Done sorting");
+
+	return orderedRules;
 }
+
+module.exports = {
+	anyRuleBySignature: anyRuleBySignature,
+	elementRuleBySignature: elementRuleBySignature,
+	appearanceRuleGroups: appearanceRuleGroups,
+	addAppearanceRule: addAppearanceRule,
+	addElementRule: addElementRule,
+	orderRulesByDependencies: orderRulesByDependencies,
+};
+
+export default module.exports;
--- src/types/rules.ts
+++ src/types/rules.ts
@@ -1,65 +1,71 @@
-import { CreatableInstances } from "@figma2rbx/rbx-ui"
-import { Component } from "./component"
+/// <reference types="@figma/plugin-typings/plugin-api" />
 
-export type RuleProperties = { [key: string]: any }
+import { CreatableInstances } from "@figma2rbx/rbx-ui";
+import { Component } from "./component";
 
-export interface RuleRejected {
-	result: "Rejected"
-	reason: string
-	offendingNodes: SceneNode[]
-	fix?: () => void
+type RuleProperties = { [key: string]: any };
+
+interface RuleRejected {
+	result: "Rejected";
+	reason: string;
+	offendingNodes: SceneNode[];
+	fix?: () => void;
 }
 
-export interface RuleContinued {
-	result: "Continued"
-	properties: RuleProperties
+interface RuleContinued {
+	result: "Continued";
+	properties: RuleProperties;
 }
 
-export interface RuleCompleted {
-	result: "Completed"
+interface RuleCompleted {
+	result: "Completed";
 }
 
 interface InstanceDefined extends RuleCompleted {
 	instance: CreatableInstances[keyof CreatableInstances];
-	component?: never
+	component?: never;
 }
 
 interface ComponentDefined extends RuleCompleted {
-	component: Component
-	instance?: never
+	component: Component;
+	instance?: never;
 }
 
-export type ElementRuleCompleted = InstanceDefined | ComponentDefined;
+type ElementRuleCompleted = InstanceDefined | ComponentDefined;
 
-// export type ElementRuleEvaluation = ElementRuleCompleted | RuleContinued | RuleRejected;
-// export type AppearanceRuleEvaluation = RuleCompleted | RuleContinued | RuleRejected;
-
-type EnumRuleOptionMember = {
-	name: string,
-	explanation: string,
+export const signatureSymbol: unique symbol = Symbol("Rule Signature");
+export interface RuleSignature {
+	name: string;
+	signatureId: string;
+	symbol: typeof signatureSymbol;
+	appearanceGroup?: string;
 }
 
-export interface EnumRuleOption {
-	type: "enum",
-	description: string,
-	members: [EnumRuleOptionMember, EnumRuleOptionMember, ...EnumRuleOptionMember[]]
+// Utility types to enforce at least 2 enum members
+type DictionaryEntry<Key extends PropertyKey, Value> = {
+	[P in Key]: Value;
 };
+type AtLeastTwoEntries<K extends PropertyKey, V> = [
+	DictionaryEntry<K, V>,
+	DictionaryEntry<K, V>,
+	...Array<DictionaryEntry<K, V>>,
+];
+type DictionaryWithAtLeastTwoEntries<Key extends PropertyKey, Value> = AtLeastTwoEntries<Key, Value>[number];
+interface EnumRuleOption {
+	type: "enum";
+	description: string;
+	members: DictionaryWithAtLeastTwoEntries<string, string>;
+}
 
-export interface BoolRuleOption {
-	type: "bool",
-	description: string,
-	defaultValue: boolean,
+interface BoolRuleOption {
+	type: "bool";
+	description: string;
+	trueExplanation: string;
+	falseExplanation: string;
+	defaultValue: boolean;
 }
 
 type RuleOptions = { [key: string]: EnumRuleOption | BoolRuleOption };
-
-export const signatureSymbol: unique symbol = Symbol("Rule Signature")
-export interface RuleSignature {
-	name: string,
-	signatureId: string,
-	symbol: typeof signatureSymbol,
-	appearanceGroup?: string,
-}
 
 type BaseRuleConfig = {
 	name: string;
@@ -67,31 +73,54 @@ type BaseRuleConfig = {
 	options?: RuleOptions;
 	evaluateAfter?: RuleSignature[];
 	evaluateBefore?: RuleSignature[];
-}
+};
 
 // Use union types to ensure that at least one of the canReject, canContinue, or canComplete properties are true
-type BaseConfigWithAtLeastRejectSetTrue = BaseRuleConfig & { canReject: true, canContinue?: boolean, canComplete?: boolean };
-type BaseConfigWithAtLeastContinueSetTrue = BaseRuleConfig & { canReject?: boolean, canContinue: true, canComplete?: boolean };
-type BaseConfigWithAtLeastCompleteSetTrue = BaseRuleConfig & { canReject?: boolean, canContinue?: boolean, canComplete: true };
+type BaseConfigWithAtLeastRejectSetTrue = BaseRuleConfig & {
+	canReject: true;
+	canContinue?: boolean;
+	canComplete?: boolean;
+};
+type BaseConfigWithAtLeastContinueSetTrue = BaseRuleConfig & {
+	canReject?: boolean;
+	canContinue: true;
+	canComplete?: boolean;
+};
+type BaseConfigWithAtLeastCompleteSetTrue = BaseRuleConfig & {
+	canReject?: boolean;
+	canContinue?: boolean;
+	canComplete: true;
+};
 
-export type RuleConfig = BaseConfigWithAtLeastRejectSetTrue | BaseConfigWithAtLeastContinueSetTrue | BaseConfigWithAtLeastCompleteSetTrue;
+export type RuleConfig =
+	| BaseConfigWithAtLeastRejectSetTrue
+	| BaseConfigWithAtLeastContinueSetTrue
+	| BaseConfigWithAtLeastCompleteSetTrue;
 
-// Generics to extract the options interface based on the provided config. This
+// Generic to extract the options interface based on the provided config. This
 // will be used to enforce that the options object passed to the evaluator
 // function is correctly typed based on the provided config, which is a nice
 // ergonomics feature.
-type GetOptionsInterface<Options extends RuleConfig['options']> = {
-	[key in keyof Options]: Options[key] extends EnumRuleOption ? keyof Options[key]["members"] : boolean
-}
-
-// Generic type to type the evluator function based on the provided config.
-export type TypeEvaluator<Config extends RuleConfig, ReturnType> = Config['options'] extends {}
-	? (node: SceneNode, properties: RuleProperties, options: GetOptionsInterface<Config['options']>) => ReturnType
-	: (node: SceneNode, properties: RuleProperties) => ReturnType
+export type GetOptionsInterface<Options extends RuleOptions> = {
+	[key in keyof Options]: Options[key] extends EnumRuleOption ? keyof Options[key]["members"] : boolean;
+};
 
 // Generics used to enforce that the evaluator function returns the correct type
 // based on the provided config, which is a nice ergonomics feature.
-type GetBaseRuleReturnType<Config extends RuleConfig> = Config["canContinue"] extends true ? RuleContinued : never | Config["canReject"] extends true ? RuleRejected : never
-export type GetElementRuleReturnType<Config extends RuleConfig> = Config["canComplete"] extends true ? ElementRuleCompleted : never | GetBaseRuleReturnType<Config>
-export type GetApperanceRuleReturnType<Config extends RuleConfig> = Config["canComplete"] extends true ? RuleCompleted : never | GetBaseRuleReturnType<Config>
+type GetBaseRuleReturnType<Config extends RuleConfig> =
+	| (Config["canContinue"] extends true ? true : never)
+	| (Config["canReject"] extends true ? true : never)
+	| (Config["canComplete"] extends true ? true : never);
 
+export type GetElementRuleReturnType<Config extends RuleConfig> =
+	| (Config["canComplete"] extends true ? ElementRuleCompleted : never)
+	| GetBaseRuleReturnType<Config>;
+
+export type GetAppearanceRuleReturnType<Config extends RuleConfig> =
+	| (Config["canComplete"] extends true ? RuleCompleted : never)
+	| GetBaseRuleReturnType<Config>;
+
+// Generic type to type the evluator function based on the provided config.
+export type TypeEvaluator<Config extends RuleConfig, ReturnType> = Config["options"] extends {}
+	? (node: SceneNode, properties: RuleProperties, options: GetOptionsInterface<Config["options"]>) => ReturnType
+	: (node: SceneNode, properties: RuleProperties) => ReturnType;
